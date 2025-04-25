e401adcda36427a5ee73316b1807efa92c05976a
Samuel Howell <rugnirtheviking@gmail.com>
2025-04-25 03:22:25 +0200

    now you can drag on the minimap bc im just cool like that

--- Minimap.cs
+++ Minimap.cs
@@ -4,18 +4,38 @@ using System;
 public class Minimap : Control
 {
 	public BaseGameHandler _baseGameHandler;
+	private bool _isDragging = false;
 	
 	public override void _Input(InputEvent @event)
 	{
-		if (@event is InputEventMouseButton btn && btn.Pressed)
+		if (@event is InputEventMouseButton btn)
 		{
 			Vector2 mousePos = btn.Position;
-
+			
+			if (btn.Pressed)
+			{
+				_isDragging = true;
+				if (GetGlobalRect().HasPoint(mousePos))
+				{
+					Vector2 worldTarget = _baseGameHandler.MinimapClickToWorld(mousePos);
+					_baseGameHandler.SetWorldTarget(worldTarget);
+					
+				}
+			}
+			else
+			{
+				_isDragging = false;
+			}
+			
+		}
+		else if (@event is InputEventMouseMotion mm && _isDragging)
+		{
+			
+			Vector2 mousePos = mm.Position;
 			if (GetGlobalRect().HasPoint(mousePos))
 			{
 				Vector2 worldTarget = _baseGameHandler.MinimapClickToWorld(mousePos);
 				_baseGameHandler.SetWorldTarget(worldTarget);
-				
 			}
 		}
 	}
